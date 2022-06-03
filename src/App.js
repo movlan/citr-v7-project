1dfa318ad136702ee431631e836d828fc9dd7f12
Bahram Movlanov <bmovlanov@gmail.com>
2022-06-03 12:27:43 -0600

    first

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
-const App = () => {
-  return React.createElement('div', {}, React.createElement('h1', {}, 'Adopt Me!'))
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, `Name: ${props.name}`),
+    React.createElement("h2", {}, `Animal: ${props.animal}`),
+    React.createElement("h2", {}, `Breed: ${props.breed}`),
+  ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+const App = () => {
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Evee",
+      animal: "Dog",
+      breed: "Cocker Spaniel",
+    }),
+    React.createElement(Pet, {
+      name: "Malysh",
+      animal: "Cat",
+      breed: "Common Cat",
+    }),
+    React.createElement(Pet, {
+      name: "Flyer",
+      animal: "Bird",
+      breed: "Parrot",
+    })
+  );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
