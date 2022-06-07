e8727f6d0b363436a0456edb6c16f6436fde498f
Bahram Movlanov <bahram.movlanov@passportinc.com>
2022-06-07 15:38:00 -0600

    Class Components

--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,46 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { Component } from "react";
 
-const Details = () => {
-  const { id } = useParams()
-  return (
-    <div>Details page for ID: {id}</div>
-  )
+class Details extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const data = await res.json();
+
+    this.setState({ loading: false, ...data.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>Loading ...</h2>;
+    }
+
+    const { name, animal, breed, city, state, description } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} – ${breed} – ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Details
+const WrappedDetails = () => {
+  const params = useParams();
+
+  return <Details params={params} />;
+};
+
+export default WrappedDetails;
