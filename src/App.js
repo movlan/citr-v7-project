f59fbae55c46c945a5254ed44b9790395e694158
Bahram Movlanov <bahram.movlanov@passportinc.com>
2022-06-16 15:13:17 -0600

    end of ssr

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { StrictMode, useState } from "react";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -10,7 +9,6 @@ const App = () => {
   return (
     <StrictMode>
       <ThemeContext.Provider value={theme}>
-        <BrowserRouter>
           <header>
             <Link to="/">Adopt Me!</Link>
           </header>
@@ -18,10 +16,10 @@ const App = () => {
             <Route path="/details/:id" element={<Details />} />
             <Route path="/" element={<SearchParams />} />
           </Routes>
-        </BrowserRouter>
       </ThemeContext.Provider>
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById("root"));
+
+export default App;
